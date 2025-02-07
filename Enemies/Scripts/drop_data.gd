8e1f313f1b98249a9621763126395855f8353739
Ryan Johnson <4967884-mrchameleon@users.noreply.gitlab.com>
2025-02-07 13:28:33 -0500

    e20. openable chests

--- Enemies/Scripts/drop_data.gd
+++ Enemies/Scripts/drop_data.gd
@@ -3,8 +3,8 @@ class_name DropData extends Resource
 
 @export var item : ItemData
 @export_range(0, 100, 1, "suffix:%") var probability : float = 100
-@export_range(1, 10, 1, "suffix:items") var min_amount : int = 1
-@export_range(1, 10, 1, "suffix:items") var max_amount : int = 1
+@export_range(1, 100, 1, "suffix:items") var min_amount : int = 1
+@export_range(1, 100, 1, "suffix:items") var max_amount : int = 1
 
 
 
--- Player/scripts/player.gd
+++ Player/scripts/player.gd
@@ -1,10 +1,8 @@
 class_name Player extends CharacterBody2D
 
-# unused code
-# signal direction_changed( new_direction: Vector2 )
+signal direction_changed( new_direction: Vector2 )
 signal player_damaged ( hurtbox: Hurtbox )
 
-# missed a thing where this was added, but the code is in enemy that could be copied over
 const DIR_4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
 
 var cardinal_direction : Vector2 = Vector2.DOWN
@@ -44,19 +42,17 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func set_direction() -> bool:
-	var new_dir : Vector2 = cardinal_direction
 	if direction == Vector2.ZERO:
 		return false
 	
-	if direction.y == 0:
-		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
-	elif direction.x == 0:
-		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	var direction_id : int = int(round((direction+cardinal_direction * 0.1).angle() / TAU * DIR_4.size()))
+	var new_dir = DIR_4[direction_id]
 
 	if new_dir == cardinal_direction:
 		return false
 
 	cardinal_direction = new_dir
+	direction_changed.emit(new_dir)
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
 
